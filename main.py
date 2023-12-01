2c9b7a211e0b70074a1a0c2da36dcc8247bb860b
savavvss <savasvvs@mail.ru>
2023-12-01 17:45:51 +0600

    register & login

--- main.py
+++ main.py
@@ -1,12 +1,76 @@
-import flask as f
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
-app = f.Flask(__name__)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SECRET_KEY'] = 'secret_key'
+db = SQLAlchemy(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(256), nullable=False)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha512')
+
+        new_user = User(username=username, password=hashed_password)
+
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Регистрация прошла успешно!', 'success')
+            return redirect(url_for('login'))
+        except:
+            db.session.rollback()
+            flash('Ошибка регистрации. Пожалуйста, попробуйте другое имя пользователя.', 'danger')
+
+    return render_template('register.html')
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/')
 def main():
+    return render_template('index.html', logged_in=current_user.is_authenticated)
 
-    return f.render_template('index.html')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
 
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            flash('Вход выполнен успешно!', 'success')
+            return redirect(url_for('main'))
+        else:
+            flash('Неверное имя пользователя или пароль. Попробуйте снова.', 'danger')
 
+    return render_template('login.html')
 
-app.run(host='localhost', port=80, debug=True)
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Вы успешно вышли из системы.', 'info')
+    return redirect(url_for('main'))
+
+# ... (ваш текущий код)
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
